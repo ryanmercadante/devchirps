e0bd357e4d5395f0cbd82489d7beda265216e7a8
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-06-16 21:32:27 -0400

    add apollo-server package and start accounts service

--- server/src/config/apollo.js
+++ server/src/config/apollo.js
@@ -1,22 +1,3 @@
 import { ApolloServer, gql } from 'apollo-server-express'
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello() {
-      return 'world'
-    },
-  },
-}
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
-
 export default server
