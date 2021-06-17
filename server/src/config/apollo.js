f7fa8870c8914f98c21834ef645bf4d55ca8b217
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-06-16 21:39:12 -0400

    setup apollo gateway and federated schema for accounts service, update scripts

--- server/src/config/apollo.js
+++ server/src/config/apollo.js
@@ -1,3 +1,13 @@
-import { ApolloServer, gql } from 'apollo-server-express'
+import { ApolloGateway } from '@apollo/gateway'
+import { ApolloServer } from 'apollo-server-express'
+
+const gateway = new ApolloGateway({
+  serviceList: [{ name: 'accounts', url: process.env.ACCOUNTS_SERVICE_URL }],
+})
+
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+})
 
 export default server
--- server/src/services/accounts/index.js
+++ server/src/services/accounts/index.js
@@ -1,15 +1,14 @@
 import { ApolloServer } from 'apollo-server'
+import { buildFederatedSchema } from '@apollo/federation'
 import resolvers from './resolvers'
 import typeDefs from './typeDefs'
-
 ;(async () => {
   const port = process.env.ACCOUNTS_SERVICE_PORT
 
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema: buildFederatedSchema([{ typeDefs, resolvers }]),
   })
 
-  const { url } = server.listen({ port })
+  const { url } = await server.listen({ port })
   console.log(`Accounts service ready at ${url}`)
 })()
