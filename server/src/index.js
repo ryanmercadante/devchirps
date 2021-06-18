95eeece45c12373203d09130b9fc485001eb68ae
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-06-18 18:35:44 -0400

    fix issues with apollo gateway not waiting for services

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,22 @@
+import waitOn from 'wait-on'
 import app from './config/app'
 import server from './config/apollo'
 
 const port = process.env.PORT
 
-server.applyMiddleware({ app })
+const options = {
+  resources: ['tcp:4001'],
+}
 
-app.listen({ port }, () => {
-  console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`)
-})
+waitOn(options)
+  .then(() => {
+    server.applyMiddleware({ app })
+    app.listen({ port }, () => {
+      console.log(
+        `Server ready at http://localhost:${port}${server.graphqlPath}`
+      )
+    })
+  })
+  .catch((err) => {
+    console.error('ERR:', err)
+  })
