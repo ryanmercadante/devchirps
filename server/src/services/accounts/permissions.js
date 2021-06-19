062be9d9411156c65da1ebf01b4680e4b335c1f7
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-06-19 14:07:31 -0400

    add rules for other Accounts queries and mutation

--- server/src/services/accounts/permissions.js
+++ server/src/services/accounts/permissions.js
@@ -1,4 +1,4 @@
-import { rule, shield } from 'graphql-shield'
+import { and, or, rule, shield } from 'graphql-shield'
 import getPermissions from '../../lib/getPermissions'
 
 const canReadAnyAccount = rule()((parent, args, { user }, info) => {
@@ -6,10 +6,50 @@ const canReadAnyAccount = rule()((parent, args, { user }, info) => {
   return userPermissions && userPermissions.includes('read:any_account')
 })
 
+const canReadOwnAccount = rule()((parent, args, { user }, info) => {
+  const userPermissions = getPermissions(user)
+  return userPermissions && userPermissions.includes('read:own_account')
+})
+
+const canEditOwnAccount = rule()((parent, args, { user }, info) => {
+  const userPermissions = getPermissions(user)
+  return userPermissions && userPermissions.includes('edit:own_account')
+})
+
+const canBlockAccount = rule()((parent, args, { user }, info) => {
+  const userPermissions = getPermissions(user)
+  return userPermissions && userPermissions.includes('block:any_account')
+})
+
+const canPromoteAccount = rule()((parent, args, { user }, info) => {
+  const userPermissions = getPermissions(user)
+  return userPermissions && userPermissions.includes('promote:any_account')
+})
+
+const isReadingOwnAccount = rule()((parent, { id }, { user }, info) => {
+  return user.sub === id
+})
+
+const isEditingOwnAccount = rule()(
+  (parent, { where: { id } }, { user }, info) => {
+    return user.sub === id
+  }
+)
+
 const permissions = shield(
   {
     Query: {
+      account: or(
+        and(canReadOwnAccount, isReadingOwnAccount),
+        canReadAnyAccount
+      ),
       accounts: canReadAnyAccount,
+    },
+    Mutation: {
+      changeAccountBlockedStatus: canBlockAccount,
+      changeAccountModeratorRole: canPromoteAccount,
+      deleteAccount: and(canEditOwnAccount, isEditingOwnAccount),
+      updateAccount: and(canEditOwnAccount, isEditingOwnAccount),
     },
   },
   {
