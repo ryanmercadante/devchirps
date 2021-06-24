d3417e40c3d6e057b157a574b4bd70d1697a63e8
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-06-24 08:58:52 -0400

    add mongoose connection fil

---
+++ server/src/config/mongoose.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+export default function () {
+  const connectionUrl = process.env.MONGODB_URL
+
+  mongoose.connect(connectionUrl, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+
+  mongoose.connection.on('connected', () => {
+    console.log(`Mongoose default connection ready at ${connectionUrl}`)
+  })
+
+  mongoose.connection.on('error', (err) => {
+    console.log('Mongoose default connection error:', err)
+  })
+}
