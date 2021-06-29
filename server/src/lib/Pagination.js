5c199d79d4d094a3197497f4e9eff8dd6307a15c
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-06-29 18:15:53 -0400

    start on some Pagination methods

--- server/src/lib/Pagination.js
+++ server/src/lib/Pagination.js
@@ -1,16 +1,83 @@
+import { UserInputError } from 'apollo-server-express'
+
 class Pagination {
   constructor(Model) {
     this.Model = Model
   }
 
   // Get documents and cast them into the correct edge/node shape
-  async getEdges(queryArgs) {}
+  async getEdges(queryArgs) {
+    const { after, before, first, last, filter = {}, sort = {} } = queryArgs
+    let edges
+
+    // handle user input errors
+    if (!first && !last) {
+      throw new UserInputError(
+        'Provide a `first` or `last` value to pagination this connection.'
+      )
+    } else if (first && last) {
+      throw new UserInputError(
+        'Passing `first` and `last` arguments is not supported with this connection.'
+      )
+    } else if (first < 0 || last < 0) {
+      throw new UserInputError(
+        'Minimum record request for `first` and `last` arguments is 0.'
+      )
+    } else if (first > 100 || last > 100) {
+      throw new UserInputError(
+        'Maximum record request for `first` and `last` arguments is 100.'
+      )
+    }
+
+    if (first) {
+      // forward pagination happens here
+      const operator = this._getOperator(sort)
+      const queryDoc = after
+        ? await this._getFilterWithCursor(after, filter, operator, sort)
+        : filter
+
+      const docs = await this.Model.find(queryDoc)
+        .sort(sort)
+        .limit(first)
+        .exec()
+
+      edges = docs.length
+        ? docs.map((doc) => ({ cursor: doc._id, node: doc }))
+        : []
+    } else {
+      // backward pagination happens here (later)
+    }
+  }
 
   // Get pagination information
-  async getPageInfo(edges, queryArgs) {}
+  async getPageInfo(edges, queryArgs) {
+    return {
+      hasNextPage: false,
+      hasPreviousPage: false,
+      startCursor: null,
+      endCursor: null,
+    }
+  }
 
   // Add the cursor ID with the correct comparison operator to the query filter
-  async _getFilterWithCursor(fromCursorId, filter, operator, sort) {}
+  async _getFilterWithCursor(fromCursorId, filter, operator, sort) {
+    let filterWithCursor = { $and: [filter] }
+    const fieldArr = Object.keys(sort)
+    const field = fieldArr.length ? fieldArr[0] : '_id'
+    const fromDoc = await this.Model.findOne({ _id: fromCursorId })
+      .select(field)
+      .exec()
+
+    if (!fromDoc) {
+      throw new UserInputError(`No record found for ID '${fromCursorId}'`)
+    }
+
+    filterWithCursor.$and.push({
+      [field]: { [operator]: fromDoc[field] },
+    })
+
+    return filterWithCursor
+  }
 
   // Create the aggregation pipeline to paginate a full-text search
   async _getSearchPipeline(fromCursorId, filter, first, operator, sort) {}
@@ -20,7 +87,10 @@ class Pagination {
   _reverseSortDirection(sort) {}
 
   // Get the correct comparison operator based on the sort order
-  _getOperator(sort, options = {}) {}
+  _getOperator(sort, options = {}) {
+    const orderArr = Object.values(sort)
+    return orderArr.length && orderArr[0] === '-1' ? '$lt' : '$gt'
+  }
 
   // Determine if a query is a full-text search based on the sort expression
   _isSearchQuery(sort) {}
