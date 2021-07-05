727c6cebd03041d11fb377256f14a8181a87dcdd
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-07-05 09:46:42 -0400

    add queries and inputs to Post typedefs

--- server/src/services/content/typeDefs.js
+++ server/src/services/content/typeDefs.js
@@ -23,6 +23,95 @@ const typeDefs = gql`
     "The body content of the post (max. 256 characters)."
     text: String!
   }
+
+  extend type Profile @key(fields: "id") {
+    id: ID! @external
+    "A list of posts written by the user."
+    posts(
+      after: String
+      before: String
+      first: Int
+      last: Int
+      orderBy: PostOrderByInput
+    ): PostConnection
+  }
+
+  """
+  Information about pagination in a connection.
+  """
+  type PageInfo {
+    "The cursor to continue from when paginating forward."
+    endCursor: String
+    "Whether there are more items when paginating forward."
+    hasNextPage: Boolean!
+    "Whether there are more items when paginating backward."
+    hasPreviousPage: Boolean!
+    "The cursor to continue from them paginating backward."
+    startCursor: String
+  }
+
+  """
+  A list of post edges with pagination information.
+  """
+  type PostConnection {
+    "A list of post edges."
+    edges: [PostEdge]
+    "Information to assist with pagination."
+    pageInfo: PageInfo!
+  }
+
+  """
+  A single post node with its cursor.
+  """
+  type PostEdge {
+    "A cursor for use in pagination."
+    cursor: ID!
+    "A post at the end of an edge."
+    node: Post!
+  }
+
+  """
+  Sorting options for post connections.
+  """
+  enum PostOrderByInput {
+    "Order posts ascending by creation time."
+    createdAt_ASC
+    "Order posts descending by creation time."
+    createdAt_DESC
+  }
+
+  """
+  Provides a filter on which posts may be queried.
+  """
+  input PostWhereInput {
+    """
+    The unique username of the user viewing the posts by users they follow.
+
+    Results includes their own posts.
+    """
+    followedBy: String
+    """
+    Whether to include posts that have been blocked by a moderator.
+
+    Default is true.
+    """
+    includeBlocked: Boolean
+  }
+
+  extend type Query {
+    "Retrieves a single post by MongoDB document ID."
+    post(id: ID!): Post!
+
+    "Retrieves a list of posts."
+    posts(
+      after: String
+      before: String
+      first: Int
+      last: Int
+      orderBy: PostOrderByInput
+      filter: PostWhereInput
+    ): PostConnection
+  }
 `
 
 export default typeDefs
